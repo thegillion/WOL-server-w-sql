37535c053b2e86c9ed257f7c2cf22d8ff919580e
thegillion <thegillion@gmail.com>
2015-01-28 15:56:16 -0500

    Change SQL

--- wolserver.sql
+++ wolserver.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 28, 2015 at 09:17 AM
--- Server version: 5.5.39-MariaDB
--- PHP Version: 5.5.20
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2015 at 09:55 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,18 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `devices` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `devicename` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `macaddress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `ipaddress` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+  `ipaddress` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `devicename` (`devicename`,`macaddress`,`ipaddress`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `devices`
 --
 
 INSERT INTO `devices` (`id`, `devicename`, `macaddress`, `ipaddress`) VALUES
-(1, 'JonesPC', 'bc:30:5b:d2:02:85', '192.168.0.25');
+(5, 'Hal 9000', '00:00:00:00:00', '0.0.0.0');
 
 -- --------------------------------------------------------
 
@@ -47,52 +49,22 @@ INSERT INTO `devices` (`id`, `devicename`, `macaddress`, `ipaddress`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` char(64) COLLATE utf8_unicode_ci NOT NULL,
   `salt` char(16) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`password`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `salt`, `email`) VALUES
-(1, 'raymond', 'bdd7a6f0ee399ce74be5a40ea41d89f8ad0e28d9462a8291e12c4ac084379494', '407dceec154ce342', 'filesaverspc@gmail.com'),
-(2, 'admin', 'd40b41204ae825f499d44f4071786b0c3e0d53ffea9931f72b87dea72e5c7b05', '7d08ad29520e3e56', 'admin@whatever.com'),
-(7, 'garrett', 'a4d62cb57f6c5c0e1324fe34b7897b07193c9abc5c56cb4f6289198f3bf2b042', '5d86ddc31d4f0420', 'raymondella2001@gmail.com');
+(2, 'admin', 'd40b41204ae825f499d44f4071786b0c3e0d53ffea9931f72b87dea72e5c7b05', '7d08ad29520e3e56', 'admin@whatever.com');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `devices`
---
-ALTER TABLE `devices`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `devicename` (`devicename`,`macaddress`,`ipaddress`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`,`password`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `devices`
---
-ALTER TABLE `devices`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
